ef1fc11b9a6ab634d089405cc4bfac3957478b77
maafik <maafik@66.ru>
2025-11-19 01:17:57 +0300

    656

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,20 +58,6 @@
     });
   });
 
-  /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-        preloader.classList.add('loaded');
-      }, 1000);
-      setTimeout(() => {
-        preloader.remove();
-      }, 2000);
-    });
-  }
 
   /**
    * Scroll top button
